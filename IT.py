eb9de132138759a132e15a6be3b797931ab1612c
Rainy <toukumpao999@gmail.com>
2025-08-31 09:26:30 +0700

    Your descriptive commit message here

---
+++ IT.py
@@ -0,0 +1,11 @@
+#print ("Hello my name is \"kumpao\"")
+#print ("\\Purpose of Learning Python\\")
+#print ("\t -I want to be Game Develop \n \t -I want to make my Own Webside in the internet \n \t -Understand about IT")
+#print("Form \"IT Step Academy - Python Core\"") 
+
+age = int(input("Enter your age: "))
+                
+if age >= 18:
+    print("You are adult")
+    
+print(age)
---
+++ helloword.py
@@ -0,0 +1,2 @@
+print("Hello World")
+print("\\How to buy Good Computer\\ \n \t - Brand computer \n \t -\" RAM \" \n \t -\" CPU \" \n \t - Computer Style \n \t - Money \n \t - Testing Compute")
